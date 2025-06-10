8b7c16834a2d3fa27efcbd873bf3b9d307c4ffcd
kunda <kunda@Kundan>
2025-06-10 23:43:07 +0530

    This is my first Commit

---
+++ src/P2.java
@@ -0,0 +1,17 @@
+public class P2 {
+
+	public static void main(String args[])
+    {
+        int x = 1;
+  
+        
+        while (x <= 5)
+        {
+            System.out.println("Value of x:" + x);
+  
+            x++;
+        }
+        
+    }
+
+}
